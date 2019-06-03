7259c4fcc68b010187f786380f7a7310fdd9ea43
aristide.cuny <aristide.cuny@epita.fr>
2019-06-03 20:06:53 +0200

    imlate

--- TBOtest2/Assets/Scripts/MenuSettings/EndgameMenu.cs
+++ TBOtest2/Assets/Scripts/MenuSettings/EndgameMenu.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class EndgameMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject WinButton;
+    public GameObject LoseButton;
 
-    // Update is called once per frame
-    void Update()
+    /*void Start()
     {
-        
-    }
+        if()
+    }*/
 }
